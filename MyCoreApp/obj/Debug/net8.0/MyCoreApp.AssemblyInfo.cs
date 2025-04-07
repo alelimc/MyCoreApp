c9b059eca4ce6b68a8143436397b7b1c3d449702
Aleli Macapagal <amacapag@my.centennialcollege.ca>
2025-04-07 02:53:26 -0400

    Initial commit

--- MyCoreApp/obj/Debug/net8.0/MyCoreApp.AssemblyInfo.cs
+++ MyCoreApp/obj/Debug/net8.0/MyCoreApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1386393e55d11e496ec6746146516876e1da541")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+624607683ca85dddab5250088afa3012335962f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
